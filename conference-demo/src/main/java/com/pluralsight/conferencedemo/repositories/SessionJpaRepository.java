2deb5ed3b4cc6d41106ef0a993e1479cfaf1dbf9
dlbunker <mcirque@gmail.com>
2020-11-03 21:45:57 -0700

    m3 changes

--- conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SessionJpaRepository.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/SessionJpaRepository.java
@@ -6,9 +6,11 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(path = "conference_sessions", collectionResourceRel = "conference_sessions")
 public interface SessionJpaRepository extends JpaRepository<Session, Long>, SessionCustomJpaRepository {
     List<Session> findBySessionNameContains(String name);
     List<Session> findBySessionNameNotLike(String name);
--- conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/TicketTypeJpaRepository.java
+++ conference-demo/src/main/java/com/pluralsight/conferencedemo/repositories/TicketTypeJpaRepository.java
@@ -2,9 +2,11 @@ package com.pluralsight.conferencedemo.repositories;
 
 import com.pluralsight.conferencedemo.models.TicketType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.List;
 
+@RepositoryRestResource(exported=false)
 public interface TicketTypeJpaRepository extends JpaRepository<TicketType, String> {
     List<TicketType> findByIncludesWorkshopTrue();
 }
